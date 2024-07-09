3f8004f75806bc7767dcd27fb7b646f4d674ff95
Eduard <eduard.lisovskij@gmail.com>
2024-07-09 13:16:07 +0200

    add function and component to render a random spotlight piece

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import ArtPieces from "@/components/ArtPieces";
+import Spotlight from "@/components/Spotlight";
 import useSWR from "swr";
 
 const fetcher = (...args) => fetch(...args).then((response) => response.json());
@@ -13,8 +14,18 @@ export default function HomePage() {
   if (error) return <div>falied to load</div>;
   if (isLoading) return <div>loading...</div>;
 
+  function randomPiece(piecesArray) {
+    return piecesArray[Math.floor(Math.random() * piecesArray.length)];
+  }
+  const randomPice = randomPiece(pieces);
+
   return (
     <div>
+      <Spotlight
+        image={randomPice.imageSource}
+        title={randomPice.name}
+        artist={randomPice.artist}
+      />
       <ArtPieces pieces={pieces} />
     </div>
   );
