5b1124b628a2b7ba51d15fb78a8dcd02a7688208
Eduard <eduard.lisovskij@gmail.com>
2024-07-09 15:56:56 +0200

    add navigation and layout

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,19 @@
 import ArtPieces from "@/components/ArtPieces";
 import Spotlight from "@/components/Spotlight";
-import useSWR from "swr";
 
-const fetcher = (...args) => fetch(...args).then((response) => response.json());
-
-export default function HomePage() {
-  const {
-    data: pieces,
-    error,
-    isLoading,
-  } = useSWR("https://example-apis.vercel.app/api/art", fetcher);
-
-  if (error) return <div>falied to load</div>;
-  if (isLoading) return <div>loading...</div>;
-
-  function randomPiece(piecesArray) {
+export default function SpotlightPage({ pieces }) {
+  function getRandomPiece(piecesArray) {
     return piecesArray[Math.floor(Math.random() * piecesArray.length)];
   }
-  const randomPice = randomPiece(pieces);
+  const randomPiece = getRandomPiece(pieces);
 
   return (
     <div>
       <Spotlight
-        image={randomPice.imageSource}
-        title={randomPice.name}
-        artist={randomPice.artist}
+        image={randomPiece.imageSource}
+        title={randomPiece.name}
+        artist={randomPiece.artist}
       />
-      <ArtPieces pieces={pieces} />
     </div>
   );
 }
